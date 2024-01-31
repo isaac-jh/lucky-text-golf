3825ac688893567549490d29aea40dbbf5a10197
bingsu-kun <icetime963@gmail.com>
2024-01-31 15:44:39 +0900

    Feat: 클럽 모델 init

---
+++ engines/club_model.py
@@ -0,0 +1,50 @@
+class Club:
+    pass
+
+class Driver(Club):
+    pass
+
+class Wood(Club):
+    pass
+
+class Wood3(Wood):
+    pass
+
+class Wood5(Wood):
+    pass
+
+class Wood7(Wood):
+    pass
+
+class Iron(Club):
+    pass
+
+class Iron4(Iron):
+    pass
+
+class Iron5(Iron):
+    pass
+
+class Iron6(Iron):
+    pass
+
+class Iron7(Iron):
+    pass
+
+class Iron8(Iron):
+    pass
+
+class Iron9(Iron):
+    pass
+
+class Wedge(Club):
+    pass
+
+class Pitch(Wedge):
+    pass
+
+class Sand(Wedge):
+    pass
+
+class Putter(Club):
+    pass
