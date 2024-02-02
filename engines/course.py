42916467c754d8cbc392c491a29bbaaf155996b9
bingsu-kun <icetime963@gmail.com>
2024-02-02 15:42:31 +0900

    Fix: 텍스트 출력 속도 조절

--- engines/course.py
+++ engines/course.py
@@ -77,11 +77,11 @@ def run_course(current_user: Golfer, course: List[Hole], course_name: str):
     hole_counter = 1
     for hole in course:
         remain = hole.total_distance
-        print(f'\n\n----------<<< Hole {hole_counter} Par {hole.par} >>>----------')
+        print(f'\n\n<<<-----<<<-----<<< Hole {hole_counter} Par {hole.par} Distance {hole.total_distance}M >>>----->>>----->>>')
+        time.sleep(3)
         field = Field.TEE
         swing_counter = 0
         while remain != 0:
-            print(f'\n<<<<<<< Distance to hole : {remain} >>>>>>')
             if field == Field.GREEN:
                 club = 'Putter'
                 c = current_user.caddy_back.putter
@@ -97,21 +97,26 @@ def run_course(current_user: Golfer, course: List[Hole], course_name: str):
             else:
                 print(f'\n\n[{swing_counter + 1}] Swing with {club}')
                 distance = current_user.swing(c, field)
+            dot_sleeper(3)
+            print('💥')
 
             if distance == 0:
                 print("\n\nOhhh..... Ball is flys away....")
+                dot_sleeper(3)
                 if hole.miss_shot_rule == Field.PENALTYAREA:
                     print("\nBall goes to Penalty Area")
-                    print("\nPenalty Score + 1")
+                    print("\nScore Penalty  +1")
                     print("\nContinue at the Penalty Tee")
                     swing_counter += 2
                     remain -= int(remain * 3 / 10)
+                    if remain <= hole.green_distance + 1:
+                        remain = hole.green_distance + 5
                 else:
                     print("\nBall goes to O.B")
-                    print("\nPenalty Score + 1")
+                    print("\nScore Penalty  +1")
                     swing_counter += 2
+                time.sleep(5)
                 continue
-
 
             if club == 'Putter':
                 dot_sleeper(5)
@@ -128,22 +133,27 @@ def run_course(current_user: Golfer, course: List[Hole], course_name: str):
                 field = Field.FRINGE
             elif remain == 1:
                 print("\n\n/////////////// Concede!!! :D ///////////////")
+                time.sleep(3)
                 swing_counter += 1
                 break
             elif remain == 0:
                 print("\n\n/////////////// Hole In!!! XD ////////////////")
+                time.sleep(3)
                 break
             elif remain <= hole.green_distance:
                 field = Field.GREEN
             else:
                 field = hole.where_is_my_ball()
             print(f'\nBall is on the {field}')
+            time.sleep(3)
             
             swing_counter += 1
 
             if swing_counter >= (hole.par * 2) - 1:
                 swing_counter = hole.par * 2
                 break
+
+            print(f'\n<<<<<<< Remaining distance to hole : {remain} >>>>>>')
 
             print("\n\nMoving to ball")
             if field == Field.GREEN:
@@ -155,6 +165,9 @@ def run_course(current_user: Golfer, course: List[Hole], course_name: str):
         results.append(result)
         show_banner(result.score)
         hole_counter += 1
+
+        print('\n\nGo to next hole')
+        dot_sleeper(10)
 
     # result 종합이랑 gained exp 표시하고 유저 세이브
     total_exp = 0
