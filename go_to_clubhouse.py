4e347d70e7457ce731bba00905ce5f5757b127b5
Janghun Lee <icetime963@gmail.com>
2024-02-06 10:49:49 +0900

    Feat: result 집계 (#4)
    * Feat: result 집계
    
    * Fix: error

--- go_to_clubhouse.py
+++ go_to_clubhouse.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from engines.user import login_or_create
 from engines.course import choose_course
 from engines.utils import show_banner
@@ -13,7 +15,38 @@ while True:
     if choice == '1':
         choose_course(current_user)
     elif choice == '2':
-        print(current_user.profile())
+        profile = current_user.profile()
+        print(f'name : {profile.get("name")}')
+        print(f'level : {profile.get("level")}')
+        print(f'total exp earned : {profile.get("exp")}')
+        print(f'total clear count : {profile.get("cleared_count")}')
+
+        cleared_courses = profile.get('cleared_course_detail')
+        score_aggregation = {}
+        total_play_time: datetime = None
+
+        for courses in cleared_courses:
+            if total_play_time:
+                total_play_time += datetime.fromisoformat(courses.get('ended_at')) - datetime.fromisoformat(courses.get('started_at'))
+            else:
+                total_play_time = datetime.fromisoformat(courses.get('ended_at')) - datetime.fromisoformat(courses.get('started_at'))
+            holes = courses.get('holes')
+            for detail in holes:
+                score_aggregation[detail.get('score')] = score_aggregation.get(detail.get('score'), 0) + 1
+
+        print(f'total playtime : {total_play_time}\n')
+        for score, count in score_aggregation.items():
+            print(f"{score} : {count}")
+
+        answer = input("If you want see more details, type 'y' or 'yes'. : ")
+        if answer == 'y' or answer == 'yes':
+            for course in cleared_courses:
+                print(f'started at : {course.get("started_at")}')
+                print(f'ended at : {course.get("ended_at")}')
+                for hole in course.get('holes'):
+                    for k, v in hole.items():
+                        print(f'\t{k} : {v}')
+
     elif choice == '3':
         print("Thank you for comming :) I'll wait your return!")
         break
